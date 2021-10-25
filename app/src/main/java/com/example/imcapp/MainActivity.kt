9a7490d67655347f0a4730050b3909ad21f9ac98
GIOVANNA JOVINA CORREIA DOS SANTOS <21193379@CFP127ALUNO.LOCAL>
2021-10-25 16:59:14 -0300

    aula 25/10

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.imcapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 
 
@@ -14,10 +15,18 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
 
         val buttonEntrar = findViewById<Button>(R.id.button_entar)
+        val buttonNovaConta= findViewById<Button>(R.id.button_nova_conta)
+
         buttonEntrar.setOnClickListener{
             val abrirDashBoard = Intent(this, DashBordActivity::class.java)
             startActivity(abrirDashBoard)
 
         }
+
+        buttonNovaConta.setOnClickListener{
+            val abrirNovaConta= Intent(this,NovoUsuarioActivity::class.java)
+            startActivity(abrirNovaConta)
+        }
+
     }
 }
--- app/src/main/java/com/example/imcapp/NovoUsuarioActivity.kt
+++ app/src/main/java/com/example/imcapp/NovoUsuarioActivity.kt
@@ -2,10 +2,71 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.RadioButton
 
 class NovoUsuarioActivity : AppCompatActivity() {
+
+    lateinit var editNome: EditText
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+    lateinit var editProfissao: EditText
+    lateinit var editAltura: EditText
+    lateinit var editDataNascimento: EditText
+    lateinit var radioFeminino: RadioButton
+    lateinit var radioMasculino: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        editNome = findViewById(R.id.edit_nome)
+        editEmail = findViewById(R.id.edit_email)
+        editSenha = findViewById(R.id.edit_senha)
+        editProfissao = findViewById(R.id.edit_profissao)
+        editAltura = findViewById(R.id.edit_altura)
+        editDataNascimento = findViewById(R.id.edit_data_nascimento)
+        radioFeminino = findViewById(R.id.radio_feminino)
+        radioMasculino= findViewById(R.id.radio_masculino)
+
+
+        supportActionBar!!.title = "Nova conta"
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(validarCampos()){
+            //gravar od dados
+        }else{
+            //grava nada
+        }
+
+        return true
+    }
+
+    private fun validarCampos(): Boolean{
+
+        var valido = true
+
+        if(editEmail.text.isEmpty()){
+            editEmail.error = "Email é obrigatorio"
+            valido = false
+        }
+
+        if(editSenha.text.isEmpty()){
+            editSenha.error = "Senha é obrigatorio"
+            valido = false
+        }
+
+        return valido
+    }
+
+    //criar menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_imc_app, menu)
+        return true
     }
 }
